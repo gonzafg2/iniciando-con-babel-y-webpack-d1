9fd0cea44a1e4381b005208a18615fc646b56fa5
Gonzalo Fleming <gonzafg2@gmail.com>
2020-05-16 02:58:13 -0400

    Creo archivo de babel para compatibilidad de JS

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,17 @@ module.exports = {
   module: {
     rules: [
       {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
